026e0634e72b0dd247efb97ecde2bd966810bb81
Omar Delarosa <thedelarosa@gmail.com>
2020-02-08 14:09:58 -0500

    adding experimental midi generator stuff

--- midi.py
+++ midi.py
@@ -1,7 +1,10 @@
 import numpy as np
 from pypianoroll import Multitrack, Track
 from matplotlib import pyplot as plt
-from oned import run
+from oned import run, metrics
+from scipy.stats import entropy
+from scipy.optimize import differential_evolution as de
+import time
 
 # Create a pianoroll matrix, where the first and second axes represent time
 # and pitch, respectively, and assign a C major chord to the pianoroll
@@ -12,9 +15,76 @@ beat_duration = 16
 
 pianoroll = np.zeros((steps * beat_duration, 128))
 
-states = run(steps)
+# C notes chord
+seed_01 = np.array([
+    1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # octave 0
+    1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, # octave 1
+    1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, # octave 2
+])
 
-C_maj_scale = np.array([60, 62, 64, 65, 67, 69, 71])
+# Random
+seed_02 = np.array([
+    0., 0., 0., 0.,  1.,  1.,  1.,
+    0., 0., 0., 0.,  1.,  1., 1.,
+    1.,  1.,  1.,  1.,  1., 0.,  1.
+])
+
+# Checkers
+seed_03 = np.array([
+    1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, # octave 0
+    0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, # octave 1
+    1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, # octave 2
+])
+
+
+# kernel
+kernel = np.array([-1.0, -2.0, 0.0, 2.0, 1.0])
+# kernel = np.array([0.5, 0.5, 0.0, 0.5, 0.5])
+seed = seed_01
+
+C_maj_scale = np.array([
+    36, 38, 40, 41, 43, 45, 47, # oct 0
+    48, 50, 52, 53, 55, 57, 59, # oct 1
+    60, 62, 64, 65, 67, 69, 71, # oct 2
+])
+
+# Kernel bounds
+bounds = [(-1.0, 1.0), (-2.0, 2.0), (-3.0, 3.0), (-4.0, 4.0), (0, 0), (-4.0, 4.0), (-3.0, 3.0), (-2.0, 2.0), (-1.0, 1.0)]
+
+# Evolve min entropy kernel
+def f_min_entropy(k):
+    states = run(steps, seed, k)
+    run_metrics = metrics(states)
+    score = run_metrics["entropy_score"] * run_metrics["num_states"]
+    print("Entropy: ", score)
+    return score
+
+
+def f_max_entropy(k):
+    states = run(steps, seed, k)
+    run_metrics = metrics(states)
+    score = -(run_metrics["entropy_score"] * run_metrics["num_states"])
+    print("Entropy: ", score)
+    return score
+
+
+optimization_steps = 250
+
+result_k = de(
+    f_max_entropy, bounds, updating="deferred", workers=2, maxiter=optimization_steps
+)
+
+k_arr = result_k.x
+# k_arr = kernel
+
+print("kernel: ", k_arr)
+print("seed: ", seed)
+
+states = run(steps, seed, k_arr)
+
+run_metrics = metrics(states)
+
+print("Metrics: ", run_metrics)
 
 for t in range(steps):
     state = states[t]
@@ -24,12 +94,17 @@ for t in range(steps):
     beat_idx = t * beat_duration
     pianoroll[beat_idx, beat_list] = 100
 
+# Clear 0s
+pianoroll[0 : (steps * beat_duration), 0] = 0
+
 # Create a `pypianoroll.Track` instance
 track = Track(pianoroll=pianoroll, program=0, is_drum=False, name="my awesome piano")
 
 mt = Multitrack(tracks=[track])
 
-mt.write("./test.mid")
+ts = int(time.time())
+
+mt.write("./renderings/midi/test_{ts}.mid".format(ts=ts))
 
 # # Plot the pianoroll
 # fig, ax = track.plot()
--- oned.py
+++ oned.py
@@ -12,6 +12,66 @@ a_b = convolve(seed, kernel)
 results = []
 print("results: ")
 
+
+def encode_state(s):
+    """
+    Encode as a tuple
+    """
+    bool_arr = np.array(s, dtype=np.bool)
+    x = np.packbits(bool_arr)
+    x_t = tuple(x)
+    # x_s = str(x_t)
+    # Tuple and string representation
+    return x_t
+
+
+def decode_state(s_t):
+    """
+    Decode from tuple
+    """
+    x_t = np.unpackbits(np.array(s_t, dtype=np.uint8))
+    return x_t
+
+
+def metrics(results_arr):
+    # size = len(results_arr)
+    states_set = set()
+    states_counts = {}
+    states_distribution = {}
+    for s in results_arr:
+        s_t = encode_state(s)  # create tuple
+        s_hash = str(s_t)
+        states_set.add(s_t)
+        if s_t in states_counts:
+            states_counts[s_hash] = states_counts[s_hash] + 1
+        else:
+            states_counts[s_hash] = 1
+
+    # Num states
+    num_states = len(states_set)
+    for key in states_counts:
+        states_distribution[key] = states_counts[key] / num_states
+
+    # Calculate entropy
+    probs_of_state = []
+
+    for s in results_arr:
+        s_t = encode_state(s)  # create tuple
+        s_hash = str(s_t)
+        prob_of_state = states_distribution[s_hash]
+        probs_of_state.append(prob_of_state)
+
+    # print("Probs", probs_of_state)
+    ent_score = entropy(probs_of_state, base=num_states)
+
+    return {
+        "states_counts": states_counts,
+        "states_distribution": states_distribution,
+        "entropy_score": ent_score,
+        "num_states": num_states,
+    }
+
+
 # Normalize
 def f(x):
     x_norm = np.linalg.norm(x)
@@ -21,12 +81,12 @@ def f(x):
 
 
 def run(steps, seed=seed, kernel=kernel):
-    results = []
+    results = [seed]
     a_b = seed
     for i in range(steps):
-        a_b = f(convolve(a_b, kernel))
+        a_b = f(convolve(a_b, kernel, mode="reflect"))
         r = a_b.copy()
-        print("{}: {}".format(i, r))
+        # print("{}: {}".format(i, r))
         results.append(r)
     return results
 
