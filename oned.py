98661d5b152ac932df98845456034f27ef1925ee
Omar Delarosa <thedelarosa@gmail.com>
2020-02-15 11:22:17 -0500

    generalizing the oned automata function

--- oned.py
+++ oned.py
@@ -4,16 +4,23 @@ from scipy.stats import entropy
 from math import floor, ceil, sqrt, log
 from automata import Automata, AutomataOptions
 
+# For generating primes
+import sympy
+from itertools import combinations_with_replacement
+from functools import reduce
+
 seed = np.array([1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0])
 kernel = np.array([-1.0, -2.0, 0.0, 2.0, 1.0])
 primes_kernel = np.array([2, 3, 5])
 
 # seed = np.array([1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0])
 
-
 seed = np.array([1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0, 0, 0, 0])
 ones = np.full((0, len(seed)), 1).squeeze()
 zeros = np.full((0, len(seed)), 0).squeeze()
+
+# Assuming k = [2,3,5]
+
 
 # seed = np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
 # kernel = np.array([-1.0, -2.0, 0.0, 2.0, 1.0])
@@ -26,6 +33,28 @@ a_b = convolve(seed, kernel)
 
 results = []
 # print("results: ")
+
+
+def primes(n):
+    """
+    Return list of first n consequtive primes
+    """
+    ns = [2]
+    if n == 1:
+        return ns
+    for i in range(n - 1):
+        ns.append(sympy.nextprime(ns[-1]))
+    return ns
+
+
+def generate_combined_products(primes_list):
+    l_primes = len(primes_list)
+    c = list(combinations_with_replacement(primes_list, l_primes))
+    # dedupe
+    cc = list(map(lambda x: np.unique(x).tolist(), c))
+    ccc = np.unique(cc).tolist()
+    products = list(map(lambda x: reduce(lambda a, b: a * b, list(x)), ccc))
+    return products
 
 
 def encode_state(s):
@@ -118,19 +147,48 @@ def gol(x, k):
 #     return x
 
 
-def wolfram(x, k):
+def rule_30():
+    # States with 0 added
+
+    # kernel is 3 consequtive primes
+    k = primes(3)
+
+    # k_states is all the combinations of products of k
+    k_states = np.array(generate_combined_products(k) + [0])
+
+    # seed is initial state
+    seed = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+
+    # print("K_states", k_states)
+    # a is the "rule" bit array
+    a = np.array([True, False, False, False, True, True, True, False])
+
+    # this is the callback
+    def r30(x, k):
+        return wolfram(x, k, a, k_states)
+
+    # TODO: do something with results
+    states = run(10, seed=seed, kernel=primes_kernel, f=r30)
+
+    mets = metrics(states)
+
+    print("Metrics: ", mets)
+    # # Pretty print for LaTex
+    for s in states:
+        print(" & ".join(map(str, s.tolist())) + "\\")
+
+
+def wolfram(x, k, a, k_states=None):
+    """
+    k: kernel operator
+    a: activation
+    k_states: kernel combination space
+    """
     x_next = convolve(x, k, mode="constant")
 
-    a = k[0]
-    b = k[1]
-    c = k[2]
+    states_arr = k_states[a]
 
-    # These correspond to the 8 CA elementary masks
-    states = [a, b, c, a * b, b * c, a * b * c, a * c, 0]
-
-    rule_30 = [a, b * c, b, c]
-
-    matches = np.isin(x_next, rule_30)
+    matches = np.isin(x_next, states_arr)
     x = np.where(matches, 1, 0)
     return x
 
@@ -151,11 +209,5 @@ def run(steps, seed=seed, kernel=kernel, f=f):
 
 # Example when run as main
 if __name__ == "__main__":
-    # seed = np.array([1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0])
-    seed = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
-    # TODO: do something with results
-    states = run(10, seed=seed, kernel=primes_kernel, f=wolfram)
-    # # Pretty print for LaTex
-    # for s in states:
-    #     print(" & ".join(map(str, s.tolist())) + "\\")
+    rule_30()
 
